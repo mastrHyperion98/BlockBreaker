fd773932f2b2fb167bbee81c5f20ee740c48fe00
Steven <s_smith1998@outlook.com>
2020-09-14 21:49:29 -0400

    Paddle movement

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float currentMousePosition;
+    private const int screenWidth = 16;
+    void Start()
+    {
+        currentMousePosition = Input.mousePosition.x / Screen.width * screenWidth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = gameObject.transform.position;
+        // Every single frame we want to find the current position
+        float mousePositionX = Input.mousePosition.x / Screen.width * screenWidth;
+        float deltaX = currentMousePosition - mousePositionX;
+        if(deltaX > 0)
+              position += new Vector3(-deltaX, 0f,0f);
+        else
+            position -= new Vector3(deltaX, 0f,0f);
+
+        gameObject.transform.position = position;
+        currentMousePosition = mousePositionX;
+
+    }
+}
