d2777786456aefd77bbfeb96e2a30c3f6fe77faf
Steven <s_smith1998@outlook.com>
2020-09-24 01:27:07 -0400

    Update Project Settings & Working Collisions and Blocks destroyed

---
+++ BlockBreaker/Assets/Scripts/Block.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private float NumberOfHits = 3;
+    private int current_sprite_index = 0;
+    private SpriteRenderer _spriteRenderer;
+
+    [SerializeField] private Sprite[] array = new Sprite[3];
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("HIT DETECTED");
+             if (other.transform.tag.CompareTo("Ball") == 0)
+             {
+                 NumberOfHits--;
+                 if(NumberOfHits == 0)
+                     Destroy(this.gameObject);
+                 ChangeSprite();
+             }
+    }
+
+    private void ChangeSprite()
+    {
+        current_sprite_index = Mathf.Clamp(current_sprite_index+1, 0, array.Length-1);
+        _spriteRenderer.sprite = array[current_sprite_index];
+    }
+    
+}
--- BlockBreaker/Assets/Scripts/Paddle.cs
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -7,6 +7,7 @@ public class Paddle : MonoBehaviour
     // Start is called before the first frame update
     private const float screenWidth = 16f;
     private const float minUnit = 1f;
+    [SerializeField] private float paddle_speed = 1.0f;
     void Start()
     {
     }
@@ -16,7 +17,7 @@ public class Paddle : MonoBehaviour
     {
         
         // Every single frame we want to find the current position
-        float mousePositionX = Input.mousePosition.x / Screen.width * screenWidth;
+        float mousePositionX = (Input.mousePosition.x / Screen.width * screenWidth) * paddle_speed;
         mousePositionX = Mathf.Clamp(mousePositionX, minUnit, screenWidth-minUnit);
         Vector2 paddlePos = new Vector2(mousePositionX, transform.position.y);
         transform.position = paddlePos;
