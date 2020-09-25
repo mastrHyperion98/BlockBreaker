70a5d79b920306ef8745d74f2bcb58eadf298f68
Steven <s_smith1998@outlook.com>
2020-09-25 19:23:01 -0400

    LevelGenerator working and functional

--- BlockBreaker/Assets/Scripts/LevelGenerator.cs
+++ BlockBreaker/Assets/Scripts/LevelGenerator.cs
@@ -1,140 +1,124 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEditor;
+using UnityEngine.Scripting;
+using Random = UnityEngine.Random;
 
-public class LevelGenerator : MonoBehaviour {
-
-
-    [Range(0,100)]
-    public int iniChance;
-    [Range(1,8)]
-    public int birthLimit;
-    [Range(1,8)]
-    public int deathLimit;
-
+public class LevelGenerator : MonoBehaviour
+{
+    private float upperYBound;
+    private float lowerYBound;
+    private float leftXBound;
+    private float rightXBound;
+    private float blockWidth;
+    private float blockHeight;
     [Range(1,10)]
-    public int numR;
-    private int count = 0;
-
-    private int[,] terrainMap;
-    public Vector3Int tmpSize;
-    public Tilemap topMap;
-    public Tile topTile;
-
-    int width;
-    int height;
-
-    public void doSim(int nu)
+    public int r;
+    [Range(1,10)]
+    public float aliveThreshold;
+    [Range(1,10)]
+    public float deathThreshold;
+    [Range(1, 100)]
+    public float initialOdds;
+    private int[,] map;
+    [SerializeField] private GameObject block;
+    private GameObject emptyHolder;
+    void Awake()
     {
-        clearMap(false);
-        width = tmpSize.x;
-        height = tmpSize.y;
+        Camera main = Camera.main;
+        blockWidth = block.GetComponent<SpriteRenderer>().bounds.size.x;
+        blockHeight = block.GetComponent<SpriteRenderer>().bounds.size.y;
+        rightXBound = main.ViewportToWorldPoint(new Vector3(1,0,0)).x-blockWidth/2.0f;
+        leftXBound = blockWidth/2.0f;
+        upperYBound = main.ViewportToWorldPoint(new Vector3(0,1,0)).y - blockHeight/2.0f;
+        lowerYBound = main.ViewportToWorldPoint(new Vector3(0, 0.3f, 0)).y;
+    }
 
-        if (terrainMap==null)
+    private void Start()
+    {
+        int num_occurences = r;
+        map = NewMap();
+        InitMap();
+        do
         {
-            terrainMap = new int[width, height];
-            initPos();
-        }
-
-
-        for (int i = 0; i < nu; i++)
+            map = Generate();
+            num_occurences--;
+        } while (num_occurences > 0);
+                    
+        emptyHolder =new GameObject();
+        for (int x = 0; x < map.GetLength(0); x++)
         {
-            terrainMap = genTilePos(terrainMap);
-        }
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < map.GetLength(1); y++)
             {
-                if (terrainMap[x, y] == 1)
-                    topMap.SetTile(new Vector3Int(-x, -y , 0), topTile);
+                if (map[x, y] == 1)
+                {
+                    Vector3 position = new Vector3(leftXBound + (x*blockWidth), upperYBound - (y*blockHeight),0.0f);
+                    Instantiate(block,position, block.transform.rotation, emptyHolder.transform);
+                }
             }
         }
     }
 
-    public void initPos()
+    private void Update()
     {
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                terrainMap[x, y] = Random.Range(1, 101) < iniChance ? 1 : 0;
-            }
-        }
     }
-    public int[,] genTilePos(int[,] oldMap)
+
+    private int[,] Generate()
     {
-        int[,] newMap = new int[width,height];
+        int numXBlocks = Mathf.FloorToInt((rightXBound - leftXBound) / blockWidth);
+        int numYBlocks = Mathf.FloorToInt((upperYBound - lowerYBound) / blockHeight);
+        int[,] newMap = NewMap();
         int neighb;
         BoundsInt myB = new BoundsInt(-1, -1, 0, 3, 3, 1);
 
 
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
+        for (int x = 0; x < numXBlocks; x++) {
+            for (int y = 0; y < numYBlocks; y++) {
                 neighb = 0;
                 foreach (var b in myB.allPositionsWithin)
                 {
                     if (b.x == 0 && b.y == 0) continue;
-                    if (x+b.x >= 0 && x+b.x < width && y+b.y >= 0 && y+b.y < height)
-                    {
-                        neighb += oldMap[x + b.x, y + b.y];
-                    }
+                    if (x+b.x >= 0 && x+b.x < numXBlocks && y+b.y >= 0 && y+b.y <numYBlocks) 
+                        neighb += map[x + b.x, y + b.y];
                     else
-                    {
                         neighb++;
-                    }
                 }
 
-                if (oldMap[x,y] == 1)
-                {
-                    if (neighb < deathLimit) newMap[x, y] = 0;
-
-                        else
-                        {
-                            newMap[x, y] = 1;
-
-                        }
+                if (map[x,y] == 1) {
+                    if (neighb < deathThreshold) 
+                        newMap[x, y] = 0;
+                    else 
+                        newMap[x, y] = 1;
                 }
-
-                if (oldMap[x,y] == 0)
-                {
-                    if (neighb > birthLimit) newMap[x, y] = 1;
-
-                else
-                {
-                    newMap[x, y] = 0;
-                }
+                if (map[x,y] == 0) {
+                    if (neighb > aliveThreshold)
+                        newMap[x, y] = 1;
+                    else
+                        newMap[x, y] = 0;
                 }
             }
         }
         return newMap;
     }
-    void Update () {
-
-        if (Input.GetKeyDown(KeyCode.Space))
+    private void InitMap() {
+        for (int x = 0; x < map.GetLength(0); x++)
         {
-            doSim(numR);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            clearMap(true);
+            for (int y = 0; y < map.GetLength(1); y++)
+            {
+                map[x, y] = Random.Range(1, 101) < initialOdds ? 1 : 0;
+            }
         }
     }
-    public void clearMap(bool complete)
+    private int[,] NewMap()
     {
-
-        topMap.ClearAllTiles();
-        if (complete)
-        {
-            terrainMap = null;
-        }
-
-
+        if(emptyHolder != null)
+            Destroy(emptyHolder.gameObject);
+        int numXBlocks = Mathf.FloorToInt((rightXBound - leftXBound) / blockWidth);
+        int numYBlocks = Mathf.FloorToInt((upperYBound - lowerYBound) / blockHeight);
+        return new int[numXBlocks,numYBlocks];
     }
-
-
 
 }
