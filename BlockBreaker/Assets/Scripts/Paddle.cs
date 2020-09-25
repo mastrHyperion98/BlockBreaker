5d0baaed96e10d759edd708d1c2061fc6d8da524
Steven <s_smith1998@outlook.com>
2020-09-24 23:48:28 -0400

    Update paddle movement smoothness

--- BlockBreaker/Assets/Scripts/Paddle.cs
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -8,20 +8,24 @@ public class Paddle : MonoBehaviour
     private const float screenWidth = 16f;
     private const float minUnit = 1f;
     [SerializeField] private float paddle_speed = 1.0f;
+    private Vector2 paddlePos;
     void Start()
     {
         Cursor.visible = false;
+        paddlePos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         // Every single frame we want to find the current position
         float mousePositionX = (Input.mousePosition.x / Screen.width * screenWidth) * paddle_speed;
         mousePositionX = Mathf.Clamp(mousePositionX, minUnit, screenWidth-minUnit);
-        Vector2 paddlePos = new Vector2(mousePositionX, transform.position.y);
-        transform.position = paddlePos;
+        paddlePos = new Vector2(mousePositionX, transform.position.y);
+    }
 
+    private void FixedUpdate()
+    {
+        transform.position = paddlePos;
     }
 }
